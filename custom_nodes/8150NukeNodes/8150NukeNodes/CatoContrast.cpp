fb487f396277326c3e8174af63eba361c842692d
LeftCircle <leftcircle@LeftStation.>
2026-03-30 10:31:31 -0400

    Basic sharpen and contrast

--- custom_nodes/8150NukeNodes/8150NukeNodes/CatoContrast.cpp
+++ custom_nodes/8150NukeNodes/8150NukeNodes/CatoContrast.cpp
@@ -1,7 +1,6 @@
-// Add.C
-// Copyright (c) 2009 The Foundry Visionmongers Ltd.  All Rights Reserved.
+// A very basic implementation of contrast
 
-static const char* const HELP = "Gamma";
+static const char* const HELP = "Adds a constant to a set of channels";
 
 #include "DDImage/PixelIop.h"
 #include "DDImage/Row.h"
@@ -9,15 +8,15 @@ static const char* const HELP = "Gamma";
 
 using namespace DD::Image;
 
-class CatoGamma : public PixelIop
+class CatoContrast : public PixelIop
 {
-  double value;
+  double mult = 3.0;
+  double subtract = 0.3;
+
 public:
   void in_channels(int input, ChannelSet& mask) const;
-  CatoGamma(Node* node) : PixelIop(node)
-  {
-    value = 0;
-  }
+  CatoContrast(Node* node) : PixelIop(node) {}
+  
   bool pass_transform() const { return true; }
   void pixel_engine(const Row &in, int y, int x, int r, ChannelMask, Row & out);
   virtual void knobs(Knob_Callback);
@@ -27,11 +26,10 @@ public:
   void _validate(bool);
 };
 
-void CatoGamma::_validate(bool for_real)
+void CatoContrast::_validate(bool for_real)
 {
   copy_info();
-  //for (unsigned i = 0; i < 4; i++) {
-  if (value) {
+  if (mult) {
     set_out_channels(Mask_All);
     info_.black_outside(false);
     return;
@@ -40,33 +38,30 @@ void CatoGamma::_validate(bool for_real)
   set_out_channels(Mask_None);
 }
 
-void CatoGamma::in_channels(int input, ChannelSet& mask) const
+void CatoContrast::in_channels(int input, ChannelSet& mask) const
 {
   // mask is unchanged
 }
 
-void CatoGamma::pixel_engine(const Row& in, int y, int x, int r,
+void CatoContrast::pixel_engine(const Row& in, int y, int x, int r,
                        ChannelMask channels, Row& out)
 {
-  const float gamma = 1.0 / value;
   foreach (z, channels) {
-    //const float c = value[colourIndex(z)];
     const float* inptr = in[z] + x;
     const float* END = inptr + (r - x);
     float* outptr = out.writable(z) + x;
     while (inptr < END)
-      //*outptr++ = *inptr++ + c;
-      *outptr++ = pow(*inptr++, gamma);
+      *outptr++ = mult * *inptr++ - subtract;
   }
 }
 
-void CatoGamma::knobs(Knob_Callback f)
+void CatoContrast::knobs(Knob_Callback f)
 {
-  //AColor_knob(f, value, IRange(0, 4), "value");
-  Float_knob(f, &value, "value");
+  Float_knob(f, &mult, "multiply");
+  Float_knob(f, &subtract, "subtract");
 }
 
 #include "DDImage/NukeWrapper.h"
 
-static Iop* build(Node* node) { return new NukeWrapper(new CatoGamma(node)); }
-const Iop::Description CatoGamma::d("CatoGamma", "Color/Math/CatoGamma", build);
+static Iop* build(Node* node) { return new NukeWrapper(new CatoContrast(node)); }
+const Iop::Description CatoContrast::d("CatoContrast", "Color/Math/CatoContrast", build);
